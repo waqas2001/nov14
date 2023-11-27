02c88dd1376092e573bb1462f5fa4534a466d351
Waqas Shahid <waqas2001@gmail.com>
2023-11-27 07:13:44 -0800

    Step 6: Extending Functionality of X

--- src/App.js
+++ src/App.js
@@ -1,36 +1,45 @@
 import './App.css';
 //import BookList from './BookList';
 import React, { useState } from 'react';
-//import AddBookForm from './AddBookForm';
-
-// import React, { useState } from 'react';
 
 function App() {
   const [books, setBooks] = useState([]);
-  const [newBookTitle, setNewBookTitle] = useState('');
+  const [newBook, setNewBook] = useState('');
 
-  // Event handler for submitting a new book title
+  const handleInputChange = (event) => {
+    setNewBook(event.target.value);
+  };
+
   const handleAddBook = () => {
-    if (newBookTitle.trim() !== '') {
-      // Update the state to include the new book title
-      setBooks([...books, newBookTitle]);
-      // Clear the input field
-      setNewBookTitle('');
+    if (newBook.trim() !== '') {
+      setBooks([...books, newBook]);
+      setNewBook('');
     }
   };
 
+  const handleRemoveBook = (bookToRemove) => {
+    const updatedBooks = books.filter((book) => book !== bookToRemove);
+    setBooks(updatedBooks);
+  };
+
   return (
-    <div>
+    <div className="App">
       <h1>My Favourite Books</h1>
-      <input
-        type="text"
-        value={newBookTitle}
-        onChange={(e) => setNewBookTitle(e.target.value)}
-      />
-      <button onClick={handleAddBook}>Add</button>
+      <div>
+        <input
+          type="text"
+          placeholder="Enter a new book title"
+          value={newBook}
+          onChange={handleInputChange}
+        />
+        <button onClick={handleAddBook}>Add</button>
+      </div>
       <ul>
         {books.map((book, index) => (
-          <li key={index}>{book}</li>
+          <li key={index}>
+            {book}
+            <button onClick={() => handleRemoveBook(book)}>X</button>
+          </li>
         ))}
       </ul>
     </div>
